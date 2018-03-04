9caf3d6da3dc9bb8f626da119e811d0e3ec7f8b8
svinkapeppa <erubanenko@gmail.com>
2018-03-04 13:13:28 +0300

    registration

--- app/views.py
+++ app/views.py
@@ -1,34 +1,64 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from piston import Steem
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return render_template('basic_info.html', result=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page:
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
-# rpcuser='ekhlyzov'
-# rpcpassword='P5KBaPy7XurbVwCK1CUSKDNVGVVrh29xbvXQuYczjYNbdvcw2KYK'
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
 def after_login(username, password):
     steem = Steem(node='wss://ws.testnet3.golos.io', rpcuser=username,
                   rpcpassword=password)
     account_info = steem.info()
 
     return 'Your account info{}'.format(account_info)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 app.jinja_env.globals.update(after_login=after_login)
