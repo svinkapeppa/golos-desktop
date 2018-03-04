5681a242f346795253e4d212c09af09520788c88
Clumpy Tuna <anuhinm@mail.ru>
2018-03-04 14:25:21 +0300

    auth added

--- tst.py
+++ tst.py
@@ -4,23 +4,48 @@ from piston import account
 
 class User:
 
-    def __init__(self, login="vgolos3", password="P5Hu2Vb5N2Gsrp5cWSZp4tHvaJr5cUBtmwEQJs7C6N6oU52E8q7d"):
+    def __init__(self, login="golosproject", password="qwerty12345"):
         self.steem_instance_ = Steem(node="wss://ws.testnet3.golos.io", rpcuser=login, rpcpassword=password)
         self.user_ = account.Account(account_name=login, steem_instance=self.steem_instance_)
+        print(self.user_.get('name'))
 
     def get_posts(self, limit=10,
                   sort="active",
                   category=None,
                   start=None):
-        #list of dicts
+        # list of dicts
         return self.steem_instance_.get_posts(limit=limit, sort=sort, category=category, start=start)
 
-    def get_user_posts(self, ):
-        pass
+    # cause of Golos API doesnt work
+    def get_user_posts(self):
+        return self.steem_instance_.get_blog(self.user_.get('name'))
 
-#example
+    def post(self,
+             title,
+             body,
+             category=None,
+             tags=[]):
+
+        self.steem_instance_.post(title, body,
+                                  author=self.user_.get('name'),
+                                  category=category,
+                                  tags=tags)
+# example 1
 zhenya = User()
 print(zhenya.user_)
+
+# example 2
 posts = zhenya.get_posts(sort="active")
 for post in posts:
+    print(post['id'])
     print(post.export())
+
+# example 3
+"""
+posts = zhenya.get_user_posts()
+for post in posts:
+    print(post.export())
+"""
+
+# example 4
+zhenya.post(title="Bang bang bang", body="<p> HELLO! </p>")
