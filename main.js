d1fc3a86da61ef57bdd807781a426d1743a78765
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-03-04 16:31:51 +0300

    Убрал девтулc

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ app.on('ready', function() {
     mainWindow.loadURL('http://localhost:5000/');
   
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the mainWindow is closed.
   mainWindow.on('closed', function() {
